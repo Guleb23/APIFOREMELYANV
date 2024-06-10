225ab6d826cad50fca657f5c3cf0c4d4076a8d44
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2024-06-11 02:16:33 +0300

    Добавьте файлы проекта.

---
+++ EmelyanovDiplom/Program.cs
@@ -0,0 +1,42 @@
+
+using Microsoft.EntityFrameworkCore;
+
+namespace EmelyanovDiplom
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            
+            //var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+            //var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+            //var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+            //var connectionstring = $"Server={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword};Trusted_Connection=True;";
+            builder.Services.AddDbContext<ApplicationDBContext>(options =>
+            {
+                options.UseNpgsql(builder.Configuration.GetConnectionString("DevConnection"));
+            });
+            var app = builder.Build();
+            app.UseHttpsRedirection();
+            // Configure the HTTP request pipeline.
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
+            //app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
